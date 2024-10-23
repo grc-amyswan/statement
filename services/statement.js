f780ff4ad8e7ae3eba55e07ee31737e2a587e576
Amy Swan [GRC] <msswanshine+grc@gmail.com>
2024-10-23 09:52:08 -0700

    Extract volumeCredits and turn it into the totalVolumeCredits function.

--- services/statement.js
+++ services/statement.js
@@ -1,16 +1,19 @@
+import invoices from '../data/invoices.json';
 import plays from '../data/plays.json';
+
+const invoice = invoices[0];
 
 export default function statement (invoice) {
 	let totalAmount = 0;
-	let volumeCredits = 0;
 	let result = `Statement for ${invoice.customer}\n`;
 	
 	for (let perf of invoice.performances) {
-		volumeCredits += volumeCreditsFor(perf);
 		// print line for this order
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
 		totalAmount += amountFor(perf);
 	}
+	
+	let volumeCredits = totalVolumeCredits();
 	result += `Amount owed is ${usd(totalAmount)}\n`;
 	result += `You earned ${volumeCredits} credits\n`;
 	return result;
@@ -41,6 +44,14 @@ function amountFor(aPerformance) {
 
 function playFor(aPerformance) {
 	return plays[aPerformance.playID];
+}
+
+function totalVolumeCredits() {
+	let volumeCredits = 0;
+	for (let perf of invoice.performances) {
+		volumeCredits += volumeCreditsFor(perf);
+	}
+	return volumeCredits;
 }
 
 function usd(aNumber) {
